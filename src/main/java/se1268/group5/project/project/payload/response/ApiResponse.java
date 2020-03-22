dfa0fe14603423a7f013266417d88d6ead08240c
thanh phan <thanhpvse62274@fpt.edu.vn>
2020-03-22 16:03:22 +0700

    update

---
+++ src/main/java/se1268/group5/project/project/payload/response/ApiResponse.java
@@ -0,0 +1,28 @@
+package se1268.group5.project.project.payload.response;
+
+public class ApiResponse {
+
+    private Boolean success;
+    private String message;
+
+    public ApiResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/se1268/group5/project/project/service/ShoesCategoryService.java
+++ src/main/java/se1268/group5/project/project/service/ShoesCategoryService.java
@@ -1,4 +1,9 @@
 package se1268.group5.project.project.service;
 
+import se1268.group5.project.project.payload.response.CategoryResponse;
+
+
+
 public interface ShoesCategoryService {
+    CategoryResponse getCategory();
 }
